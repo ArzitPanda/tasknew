f41f4f2cabfa752682f61e8d31502505f29a3c64
Arijit panda <73696583+ArzitPanda@users.noreply.github.com>
2025-04-13 13:34:56 +0530

    Update index.js

--- TaskManagementBackend/DbConnection/index.js
+++ TaskManagementBackend/DbConnection/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 
 const DbConnection =() =>{
 
-    const connect =  mongoose.connect("mongodb+srv://arzit:Panda2001@cluster0.ngraetz.mongodb.net/?retryWrites=true&w=majority")
+    const connect =  mongoose.connect("mongodb+srv://arzit:Panda2001@cluster0.4hcwv.mongodb.net/?retryWrites=true&w=majority")
 .then(()=>{console.log("connect sucessfully")})
 .catch(err=>console.log(err))
 }
